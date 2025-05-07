4c8cc90c25abcecb5e8b7ef10c24c8646663c0e1
mr cmuc <christian.fischer24@web.de>
2025-05-07 05:13:22 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  content: {
+    preview: {
+      // force module initialization on dev env
+      dev: true
+    }
+  },
   modules: [
     '@nuxt/content',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/ui',
   ],
   devtools: { enabled: true },
   future: {
